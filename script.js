7c585b1d98be8eb2604b0e9cd69f1ba29f9b2a2b
Louis MacConnell <louismacconnell@hotmail.com>
2019-11-26 17:27:15 +1030

    reset div clears board

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+let default_size = 16;
+
+
 function makeGrids(){
     const container = document.querySelector(".grid_container");
-    let cols = container.style.gridTemplateColumns = "repeat(16, 1fr)";
-    let rows = container.style.gridTemplateRows = "repeat(16, 1fr)";
-    for(let i = 0; i < 16 * 16; i++){
+    let cols = container.style.gridTemplateColumns = `repeat(${default_size}, 1fr)`;
+    let rows = container.style.gridTemplateRows = `repeat(${default_size}, 1fr)`;
+    for(let i = 0; i < default_size * default_size; i++){
         const div = document.createElement('div');
         div.className = "grid_item";
         //div.textContent = i;
@@ -18,7 +21,13 @@ function black(){
         });
     });
 }
-
+const button = document.querySelector("#reset");
+button.addEventListener("click",(e) => {
+    const black_grids = document.querySelectorAll(".grid_item");
+    black_grids.forEach((grid) => {
+        grid.style.background = "white";
+    });
+});
 
 
 makeGrids()
