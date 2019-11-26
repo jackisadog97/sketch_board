3fbb08fa074f09e6cfe11aa6846be65a77b6a597
Louis MacConnell <louismacconnell@hotmail.com>
2019-11-26 15:26:16 +1030

    mouseover changes grid color to black

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function makeGrids(){
     const container = document.querySelector(".grid_container");
-    console.log(container)
     let cols = container.style.gridTemplateColumns = "repeat(16, 1fr)";
     let rows = container.style.gridTemplateRows = "repeat(16, 1fr)";
     for(let i = 0; i < 16 * 16; i++){
         const div = document.createElement('div');
         div.className = "grid_item";
-        div.textContent = i;
+        //div.textContent = i;
         container.appendChild(div);
     }
-
+    black();
 }
-makeGrids();
+function black(){
+    const grid_item = document.querySelectorAll(".grid_item");
+    grid_item.forEach((item) => {
+        item.addEventListener("mouseenter",(e) => {
+            e.target.style.background = "black";
+        });
+    });
+}
+
+
+
+makeGrids()
