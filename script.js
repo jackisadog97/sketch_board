711e2cb74aff8118c35f05afb72342faa257540a
Louis MacConnell <louismacconnell@hotmail.com>
2019-11-27 11:30:54 +1030

    added colour change affect + change grid to new size

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
-let default_size = 16;
+window.onload = makeGrid(16);
 
 
-function makeGrids(){
+
+
+function makeGrid(default_size){
     const container = document.querySelector(".grid_container");
     let cols = container.style.gridTemplateColumns = `repeat(${default_size}, 1fr)`;
     let rows = container.style.gridTemplateRows = `repeat(${default_size}, 1fr)`;
-    for(let i = 0; i < default_size * default_size; i++){
-        const div = document.createElement('div');
-        div.className = "grid_item";
-        //div.textContent = i;
-        container.appendChild(div);
-    }
-    black();
+        for(let i = 0; i < default_size * default_size; i++){
+            const div = document.createElement('div');
+            div.className = "grid_item";
+            //div.textContent = i;
+            container.appendChild(div);
+        }
+        addHoverEffect();
+    
 }
-function black(){
+
+
+function addHoverEffect(setColour="#000000") {
     const grid_item = document.querySelectorAll(".grid_item");
     grid_item.forEach((item) => {
         item.addEventListener("mouseenter",(e) => {
-            e.target.style.background = "black";
+            e.target.style.background = setColour;
         });
     });
 }
-const button = document.querySelector("#reset");
-button.addEventListener("click",(e) => {
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click",() => {
     const black_grids = document.querySelectorAll(".grid_item");
     black_grids.forEach((grid) => {
         grid.style.background = "white";
     });
 });
 
+const newGrid = document.querySelector("#newGrid");
+newGrid.addEventListener("click",() => {
+    let newSize = window.prompt("How many pixels would you like on each " +
+        "side?");
+    const container = document.querySelector(".grid_container");
+    let first = container.firstChild;
 
-makeGrids()
+    let size = parseInt(newSize);
+    while(first){
+        first.remove();
+        first = container.firstChild;
+    }
+    makeGrid(size);
+});
+
+const colour = document.querySelector("#color");
+colour.addEventListener("input", () => addHoverEffect(colour.value));
+ 
+
